6ffd1b693b082e57cda6f22c3705ee690c323a9b
stLmpp <gui.stlmpp@gmail.com>
2022-10-13 15:21:32 -0300

    - prettier

--- src/operators/map-to.spec.ts
+++ src/operators/map-to.spec.ts
@@ -5,5 +5,5 @@ import { mapTo } from './map-to';
 describe('mapTo', () => {
   it('should map from one value to another', () => {
     expect(Transform.create([]).pipe(mapTo(true)).value).toBe(true);
-  })
-})
+  });
+});
